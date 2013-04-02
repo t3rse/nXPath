e8a6ca6ea8c228a39173d2af4bb2286c2fba1e3d
t3rse <david.seruyange@gmail.com>
2013-04-01 23:40:59 -0500

    Integrated Syntax Highlighter

--- trunk/nXPath/nXPath/Controllers/HomeController.cs
+++ trunk/nXPath/nXPath/Controllers/HomeController.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Web;
 using System.Web.Mvc;
+using System.Xml;
 using System.Xml.Linq;
 using System.Xml.XPath;
 using nXPath.Lib.CustomActionResult;
@@ -36,10 +38,34 @@ namespace nXPath.Controllers
 
         [HttpPost]
         public ActionResult XPathProcessing(string xmlDoc, string xPath) {
+           
+            List<string> results = new List<string>();
             var xml = Server.HtmlDecode(xmlDoc);
-            XDocument doc = XDocument.Parse(xml);
-            
-            return Content("OK");
+
+            XmlDocument xdoc = new XmlDocument();
+            xdoc.LoadXml(xml);
+
+            var nodeList = xdoc.SelectNodes(xPath);
+
+            foreach (var node in nodeList) {
+                if (node is XmlElement)
+                {
+                    results.Add((node as XmlElement).OuterXml);
+                }
+                else if (node is XmlText)
+                {
+                    results.Add((node as XmlText).Value);
+                }
+                else {
+                    results.Add(String.Format("{0}::{1}", node.GetType().ToString(), node.ToString()));
+                }
+            }
+
+            return Content(String.Join("<br/>", results.Select(r => r.ToString()).ToArray()));
+        }
+
+        public ActionResult XResolve() {
+            return View();
         }
 
     }
